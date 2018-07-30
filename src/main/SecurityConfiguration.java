a67755df37c0bf2a979e145f509937a11d1d114e
chimo jiang <33191928+chimoJ@users.noreply.github.com>
2018-07-30 09:55:41 -0700

    Create SecurityConfiguration.java

---
+++ src/main/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+Login Controller and Security Controller
+===
+SecurityConfiguration.java
+---
+    package com.in28minutes.security;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+    import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+    @Configuration
+    @EnableWebSecurity
+    public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    //who are the valid users
+	  @Autowired
+	  public void configureGlobalSecurity(AuthenticationManagerBuilder auth)
+			throws Exception {
+		auth.inMemoryAuthentication().withUser("in28Minutes").password("dummy")
+				.roles("USER", "ADMIN");//Admin Role
+	  }
+    //define which urls are secure, ex, any Url contain "todo" are secure
+	  @Override
+	  protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/login").permitAll()
+				.antMatchers("/", "/*todo*/**").access("hasRole('USER')").and()
+				.formLogin();
+	    }
+    }
